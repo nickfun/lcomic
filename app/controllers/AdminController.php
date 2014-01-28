5583329150cffd07b20d82a082cc4abb2674e86d
Nick Fun <nick@nick.gs>
2014-01-28 12:06:35 -0800

    check that user is logged in before can goto admin page

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -3,6 +3,11 @@
 class AdminController extends \BaseController {
 
     public function getIndex() {
+	if( !Auth::check() ) {
+	    // must be logged in
+	    Session::flash('msg','You must be logged in to view /admin');
+	    return Redirect::to('/auth');
+	}
 	return View::make('admin');
     }
 
